c3704eb7fc554786836489dec3949008e1cf9848
Luiz Carlos <lcpojr@gmail.com>
2020-10-03 13:24:34 -0300

    feat: add is_strong function

--- apps/resource_manager/lib/credentials/passwords.ex
+++ apps/resource_manager/lib/credentials/passwords.ex
@@ -10,14 +10,24 @@ defmodule ResourceManager.Credentials.Passwords do
 
   alias ResourceManager.Credentials.Cache
 
+  @doc "Checks if the given password is strong enough to be used"
+  @spec is_strong?(password :: String.t()) :: boolean()
+  def is_strong?(password) when is_binary(password) do
+    cond do
+      String.length(password) < 6 -> false
+      is_allowed?(password) == false -> false
+      true -> true
+    end
+  end
+
   @doc "Checks if the given password is one of the most common passwords"
   @spec is_allowed?(password :: String.t()) :: boolean()
   def is_allowed?(password) when is_binary(password) do
     password
     |> Cache.get()
     |> case do
-      nil -> false
-      _any -> true
+      nil -> true
+      _any -> false
     end
   end
 end
