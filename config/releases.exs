7f8d65dffd3f6ac8b89a91f3641d61d049bb7564
Luiz Carlos <lcpojr@gmail.com>
2020-11-28 14:50:10 -0300

    chore: add more comments to configurations

--- config/releases.exs
+++ config/releases.exs
@@ -1,5 +1,8 @@
 import Config
 
+# You can change the endpoint configurations the way it suits you better
+# This is just for us to have an exemple running on gigalixir
+# https://watcherex.gigalixirapp.com/
 config :rest_api, RestAPI.Endpoint,
   url: [host: System.fetch_env!("APP_NAME") <> ".gigalixirapp.com", port: 443],
   secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
