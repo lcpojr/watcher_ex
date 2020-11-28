9ad2162df9fcd7477a2629f51cd6f0d3bc23f870
Luiz Carlos <lcpojr@gmail.com>
2020-11-28 15:50:09 -0300

    fix: http port on releases

--- config/releases.exs
+++ config/releases.exs
@@ -4,7 +4,7 @@ import Config
 # This is just for us to have an exemple running on gigalixir
 # https://watcherex.gigalixirapp.com/
 config :rest_api, RestAPI.Endpoint,
-  ttp: [port: {:system, "PORT"}],
+  http: [port: {:system, "PORT"}],
   url: [host: System.fetch_env!("APP_NAME") <> ".gigalixirapp.com", port: 443],
   secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
   server: true
