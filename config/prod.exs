7c01f5521ee0ab4a89edc2a77d501ad3bda9ef45
Luiz Carlos <lcpojr@gmail.com>
2020-11-28 14:43:20 -0300

    chore: update database configurations

--- config/prod.exs
+++ config/prod.exs
@@ -6,10 +6,14 @@ config :logger, level: :info
 # Resource Manager
 ###################
 
-config :resource_manager, ResourceManager.Repo, url: System.fetch_env!("DATABASE_URL")
+config :resource_manager, ResourceManager.Repo,
+  url: System.fetch_env!("DATABASE_URL"),
+  pool_size: System.fetch_env!("DATABASE_POOL_SIZE")
 
 ################
 # Authenticator
 ################
 
-config :authenticator, Authenticator.Repo, url: System.fetch_env!("DATABASE_URL")
+config :authenticator, Authenticator.Repo,
+  url: System.fetch_env!("DATABASE_URL"),
+  pool_size: System.fetch_env!("DATABASE_POOL_SIZE")
