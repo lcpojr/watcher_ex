93a69e8e9178180d45332d02b7116662ce3802ba
Diogo Dourado <dcdourado@gmail.com>
2021-07-07 22:13:31 -0300

    test: get_session cache case

--- apps/authenticator/test/authenticator/sessions/commands/get_session_test.exs
+++ apps/authenticator/test/authenticator/sessions/commands/get_session_test.exs
@@ -1,12 +1,22 @@
 defmodule Authenticator.Sessions.Commands.GetSessionTest do
   use Authenticator.DataCase, async: true
 
+  import ExUnit.CaptureLog
+
   alias Authenticator.Repo
+  alias Authenticator.Sessions.Cache
   alias Authenticator.Sessions.Commands.GetSession
   alias Authenticator.Sessions.Commands.Inputs.GetSession, as: Input
   alias Authenticator.Sessions.Schemas.Session
 
   setup do
+    current_level = Logger.level()
+    Logger.configure(level: :info)
+    on_exit(fn ->
+      Logger.configure(level: current_level)
+      Cache.flush()
+    end)
+
     {:ok, session: insert!(:session)}
   end
 
@@ -20,8 +30,29 @@ defmodule Authenticator.Sessions.Commands.GetSessionTest do
         status: ctx.session.status
       }
 
-      assert {:ok, %Session{} = user} = GetSession.execute(input)
-      assert user == Repo.one(Session)
+      assert capture_log(fn ->
+        assert {:ok, %Session{} = session} = GetSession.execute(input)
+        assert session == Repo.one(Session)
+      end) =~ "Session not found on cache"
+
+
+    end
+
+    test "succeeds gettin subject session from cache if it exists", ctx do
+      Cache.set(ctx.session.jti, ctx.session)
+
+      input = %{
+        id: ctx.session.id,
+        jti: ctx.session.jti,
+        subject_id: ctx.session.subject_id,
+        subject_type: ctx.session.subject_type,
+        status: ctx.session.status
+      }
+
+      assert capture_log(fn ->
+        assert {:ok, %Session{} = session} = GetSession.execute(input)
+        assert session == Repo.one(Session)
+      end) =~ "Session #{ctx.session.id} found on cache"
     end
 
     test "fails if session does not exist" do
