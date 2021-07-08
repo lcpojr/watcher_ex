ceeaf2c22ecd0c1664f43ebd448ad477efaecfb9
Diogo Dourado <dcdourado@gmail.com>
2021-07-07 21:45:17 -0300

    test: convert_expiration/1

--- apps/authenticator/test/authenticator/sessions_test.exs
+++ apps/authenticator/test/authenticator/sessions_test.exs
@@ -90,4 +90,14 @@ defmodule Authenticator.Sessions.SessionsTest do
       end
     end
   end
+
+  describe "#{Sessions}.convert_expiration/1" do
+    test "succeeds if input is integer" do
+      assert %NaiveDateTime{} = Sessions.convert_expiration(123_456_789)
+    end
+
+    test "raises if non integer input" do
+      Sessions.convert_expiration(1.5)
+    end
+  end
 end
