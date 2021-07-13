23037c38286200c83589aac0299c75d8efe7a3c8
Luiz Carlos <lcpojr@gmail.com>
2021-07-13 15:46:11 -0300

    feat: enable server tls

--- config/dev.exs
+++ config/dev.exs
@@ -18,6 +18,12 @@ config :authenticator, Authenticator.Repo, show_sensitive_data_on_connection_err
 
 config :rest_api, RestAPI.Endpoint,
   http: [port: 4000],
+  https: [
+    port: 4001,
+    cipher_suite: :strong,
+    certfile: "priv/cert/selfsigned.pem",
+    keyfile: "priv/cert/selfsigned_key.pem"
+  ],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
